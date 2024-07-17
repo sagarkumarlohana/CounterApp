cf9bfccf1ed2d46c59b2d04f059e5126bae5f6f4
sagarkumarlohana <138721656+sagarkumarlohana@users.noreply.github.com>
2024-07-17 12:59:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// let countEL= document.getElementById('count-el');
+// let pr= document.getElementById('pre-ent');
+// console.log(pr);
+// let count=0;
+
+// function increment(){
+//     count=count+1;
+//     countEL.innerText= count;
+// }
+
+// function save(){
+
+//     let countsrt= count+ "-"
+
+//     pr.innerText+=countsrt;
+
+//     console.log(count);
+
+// }
